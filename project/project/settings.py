b52a303f464a5423a96700fa4e7ea5a5e9bda9fa
Ular <eldarakashka@gmail.com>
2025-04-11 14:48:32 +0600

    add 10.100.191.9 in hosts

--- project/project/settings.py
+++ project/project/settings.py
@@ -17,7 +17,7 @@ dotenv.load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cert.gov.kg', '10.100.191.8', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['cert.gov.kg', '10.100.191.9', '10.100.191.8', '127.0.0.1', 'localhost']
 
 
 # Application definition
