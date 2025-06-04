35c490d551272dc9a17a36a928c21adfac7270bb
Ular <eldarakashka@gmail.com>
2025-06-04 11:14:17 +0600

    change settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -254,15 +254,12 @@ LOGGING = {
 DJANGO_DB_LOGGER_ENABLE_FORMATTER = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail.cert.gov.kg'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-
-EMAIL_HOST_USER = 'eldarakashka@gmail.com'
-EMAIL_HOST_PASSWORD = 'jybo nrxv ijwm drwo'
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 APPEAL_EMAIL = ''
 
 # captcha google
