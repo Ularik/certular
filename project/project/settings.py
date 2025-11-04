e1c3bdb2afac1c1c9d6eb0e2d94cfa6267fb2a46
Ular <eldarakashka@gmail.com>
2025-11-04 09:44:12 +0600

    change debug

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,7 +14,7 @@ dotenv.load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cert.gov.kg', '10.100.191.9', '10.100.191.8', '127.0.0.1', 'localhost']
 
