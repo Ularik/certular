9a9b49eeccbb23217255bde00b4e0fca3a35af3d
Ular <eldarakashka@gmail.com>
2026-06-15 14:57:21 +0600

    убрал страницу

--- project/szi/views.py
+++ project/szi/views.py
@@ -1,30 +1,9 @@
 from django.shortcuts import render
 import logging
-from .models import CryptoProvider, NpaLinks, Applicant, EncryptionTools
-from pprint import pprint
+from .models import CryptoProvider, EncryptionTools
+
 
 logger = logging.getLogger(__name__)
-
-
-def crypto_szi_view(request):
-    crypto_szi = CryptoProvider.objects.filter(title='szi').first()
-    act_files = crypto_szi.crypto_npa.all()
-    text = crypto_szi.description.split('\n')
-    act_links = crypto_szi.npa_links.all()
-
-    context = {}
-    context['title'] = crypto_szi.name
-    context['paragraph_first'] = text[0]
-    context['paragraph_second'] = text[2]
-    context['paragraph_third'] = text[3]
-    context['paragraph_fourth'] = text[4]
-    context['paragraph_fifth'] = text[5]
-    context['paragraph_sixth'] = text[6]
-    context['acts_start'] = text[1]
-    context['acts_links'] = act_links
-    context['act_files'] = act_files
-
-    return render(request, 'crypto_provider/szi.html', context)
 
 
 def crypto_signature_view(request):
